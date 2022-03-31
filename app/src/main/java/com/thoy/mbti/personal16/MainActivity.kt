143fcd9eb5ad27e5773a80f28bfd20b7a50a2dfa
ttttthoy <tremexplo8@daum.net>
2022-03-31 20:32:16 +0900

    binding

--- app/src/main/java/com/thoy/mbti/personal16/MainActivity.kt
+++ app/src/main/java/com/thoy/mbti/personal16/MainActivity.kt
@@ -6,32 +6,44 @@ import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.webkit.WebViewClient
+import com.thoy.mbti.personal16.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var mBinding: ActivityMainBinding? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(findViewById(R.id.toolbar))
 
-        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
+        //setSupportActionBar(findViewById(R.id.toolbar))
+
+        webView.apply {
+            webViewClient = WebViewClient()
+            settings.javaScriptEnabled = true
+        }
+
+        webView.loadUrl("https://tremexplo.tistory.com")
+
+/*        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
+        }*/
+    }
+
+    override fun onBackPressed() {
+        //super.onBackPressed()
+        if (webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            finish()
         }
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+/*    override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.menu_main, menu)
         return true
-    }
+    }*/
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
 }
